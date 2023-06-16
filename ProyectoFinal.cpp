ba07d4708d2cc95d4a24f17745c2574703b30ef1
Contr3ras07 <127340114+Contr3ras07@users.noreply.github.com>
2023-06-16 10:30:27 -0600

    Add files via upload
    RamaMain

---
+++ ProyectoFinal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//TEMA: SISTEMA DE RESERVA DE LABORATORIOS.
+
+//ESTRUCTURAS.
+
+
+//PROTOTIPOS DE FUNCIONES.
+
+//FUNCION MAIN.
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+
+
+//FUNCIONES.
